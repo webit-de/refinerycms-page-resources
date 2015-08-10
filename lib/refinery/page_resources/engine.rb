d092a8c8320b643ad9f8159f5bde919329848445
mhtremblay <mhtremblay@bisscomm.com>
2015-08-10 09:18:46 -0400

    add asset precompile in engine

--- lib/refinery/page_resources/engine.rb
+++ lib/refinery/page_resources/engine.rb
@@ -34,6 +34,7 @@ module Refinery
 
       config.to_prepare do
         Refinery::PageResources.attach!
+        Rails.application.config.assets.precompile += %w( refinery/page-resource-picker.css)
       end
 
       config.after_initialize do
